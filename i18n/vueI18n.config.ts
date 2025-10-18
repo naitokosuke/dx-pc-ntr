04c9ea16a7ca12aac0bc175ceae8358f708db197
naitokosuke <kosuke.naito.engineer@gmail.com>
2025-10-18 15:33:03 +0900

    I18n

---
+++ i18n/vueI18n.config.ts
@@ -0,0 +1,23 @@
+export default {
+  legacy: false,
+  locale: "ja",
+  fallbackLocale: "ja",
+  datetimeFormats: {
+    ja: {
+      short: { year: "numeric", month: "2-digit", day: "2-digit" },
+      long: { year: "numeric", month: "long", day: "numeric", weekday: "long" },
+    },
+    en: {
+      short: { year: "numeric", month: "2-digit", day: "2-digit" },
+      long: { year: "numeric", month: "long", day: "numeric", weekday: "long" },
+    },
+  },
+  numberFormats: {
+    ja: {
+      currency: { style: "currency", currency: "JPY" },
+    },
+    en: {
+      currency: { style: "currency", currency: "USD" },
+    },
+  },
+};
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
     "@pinia/nuxt",
     "@pinia/colada-nuxt",
     "nuxt-typed-router",
+    "@nuxtjs/i18n",
   ],
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
@@ -20,5 +21,15 @@ export default defineNuxtConfig({
         semi: true,
       },
     },
+  },
+  i18n: {
+    defaultLocale: "ja",
+    locales: [
+      { code: "ja", language: "ja-JP", name: "日本語", files: ["ja.yaml"] },
+      { code: "en", language: "en-US", name: "English", files: ["en.yaml"] },
+    ],
+    langDir: "locales",
+    vueI18n: "./vueI18n.config.ts",
+    strategy: "prefix_except_default",
   },
 });
