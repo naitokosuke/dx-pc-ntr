05c2726ae3e2fa85f1179b0855b18d0c4743df21
naitokosuke <kosuke.naito.engineer@gmail.com>
2025-10-18 15:02:48 +0900

    nlx nuxi@latest module add pinia pinia-colada

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "@pinia/nuxt", "@pinia/colada-nuxt"],
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   devServer: {
