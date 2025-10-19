bea2af64073ae9039a3f7922849c7c182f9d0735
naitokosuke <kosuke.naito.engineer@gmail.com>
2025-10-19 18:57:09 +0900

    nid PC Devtools

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import { PiniaColadaDevtools } from "@pinia/colada-devtools";
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
+  <PiniaColadaDevtools />
 </template>
